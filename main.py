a5934be16fca8462d82d620e43218984ce4f9b26
F4T00T42 <ahmedash99242@gmail.com>
2025-12-04 17:15:30 +0200

    Created a screen and board functions

--- main.py
+++ main.py
@@ -21,3 +21,25 @@ SQUARE_SIZE = WIDTH // BOARD_COLS
 CIRCLE_RADIUS = SQUARE_SIZE // 3
 CIRCLE_WIDTH = 15
 CROSS_WIDTH = 25
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('AI Tic Tac Toe')
+screen.fill(BLACK)
+
+board = np.zeros((BOARD_ROWS, BOARD_COLS))
+
+
+def Lines(color = WHITE):
+  for i in range(1, BOARD_ROWS):
+    pygame.draw.line(screen, color, (0, SQUARE_SIZE * i), (WIDTH, SQUARE_SIZE * i), LINE_WIDTH)
+    pygame.draw.line(screen, color, (SQUARE_SIZE * i, 0), (HEIGHT, SQUARE_SIZE * i), LINE_WIDTH)
+
+
+def DrawFigures(color = WHITE):
+  for row in range(BOARD_ROWS):
+    for col in range(BOARD_COLS):
+      if board[row][col] == 1:
+        pygame.draw.line(screen, color, (int(col * SQUARE_SIZE), int(row * SQUARE_SIZE)), (int(col * SQUARE_SIZE + SQUARE_SIZE), int(row * SQUARE_SIZE + SQUARE_SIZE)))
+        pygame.draw.line(screen, color, (int(col * SQUARE_SIZE + SQUARE_SIZE), int(row * SQUARE_SIZE)), (int(col * SQUARE_SIZE), int(row * SQUARE_SIZE + SQUARE_SIZE)))
+      elif board[row][col] == 2:
+        pygame.draw.circle(screen, color, (int(col * SQUARE_SIZE + SQUARE_SIZE // 2), int(row * SQUARE_SIZE + SQUARE_SIZE // 2)), CIRCLE_RADIUS, CIRCLE_WIDTH)
