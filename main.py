b070fb5457b857cc7fcc7fe3d6ea321772dcaa99
F4T00T42 <ahmedash99242@gmail.com>
2025-12-06 10:24:55 +0200

    Expand Tic Tac Toe from 3x3 to 4x4x4
    - Updated Minimax data to solve 4x4x4
    - Updated board data structure to 4x4x4
    - Adjusted win-condition checks for 4 in a row in 3 dimensions
    - Updated input for 4x4x4 coordinates
    - Modified display logic for 4x4x4 board

--- main.py
+++ main.py
@@ -12,75 +12,115 @@ GREEN = (0, 255, 0)
 GRAY = (180, 180, 180)
 
 #SIZES
-HEIGHT = 400
-WIDTH = 400
+HEIGHT = 350
+WIDTH = 1400
 LINE_WIDTH = 5
+BOARD_DIMENSIONS = 4
 BOARD_ROWS = 4
 BOARD_COLS = 4
-SQUARE_SIZE = WIDTH // BOARD_COLS
-CIRCLE_RADIUS = SQUARE_SIZE // 4
-CIRCLE_WIDTH = 10
-CROSS_WIDTH = 20
+SQUARE_WIDTH = WIDTH // 16
+SQUARE_HEIGHT = HEIGHT // 4
+SQUARE_SIZE = 87
+DIMENSION_SIZE = SQUARE_WIDTH * 4
+CIRCLE_RADIUS = SQUARE_WIDTH // 4
+CIRCLE_WIDTH = 5
+CROSS_WIDTH = 10
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('AI Tic Tac Toe')
 screen.fill(BLACK)
 
-board = np.zeros((BOARD_ROWS, BOARD_COLS))
+board = np.zeros((BOARD_DIMENSIONS, BOARD_ROWS, BOARD_COLS))
+
+maxDepth = 1
 
 
+#BOARD
 def drawLines(color = WHITE):
-  for i in range(1, BOARD_ROWS):
-    pygame.draw.line(screen, color, (0, SQUARE_SIZE * i), (WIDTH, SQUARE_SIZE * i), LINE_WIDTH)
-    pygame.draw.line(screen, color, (SQUARE_SIZE * i, 0), (SQUARE_SIZE * i, HEIGHT), LINE_WIDTH)
-
+  for i in range(1, BOARD_DIMENSIONS):
+    pygame.draw.line(screen, color, (DIMENSION_SIZE * i, 0), (DIMENSION_SIZE * i, HEIGHT), LINE_WIDTH * 3)
+  for y in range(BOARD_DIMENSIONS):
+    for i in range(1, BOARD_COLS):
+      pygame.draw.line(screen, color, (0, SQUARE_HEIGHT * i), (WIDTH, SQUARE_HEIGHT * i), LINE_WIDTH) #HORIZONTAL LINES
+      pygame.draw.line(screen, color, (DIMENSION_SIZE * y + SQUARE_WIDTH * i, 0), (DIMENSION_SIZE * y + SQUARE_WIDTH * i, WIDTH), LINE_WIDTH) #VERTICAL LINES
 
 def drawFigures(color = WHITE):
-  for row in range(BOARD_ROWS):
-    for col in range(BOARD_COLS):
-      if board[row][col] == 1:
-        pygame.draw.line(screen, color, (col * SQUARE_SIZE + SQUARE_SIZE // 4, row * SQUARE_SIZE + SQUARE_SIZE // 4), (col * SQUARE_SIZE + 3 * SQUARE_SIZE // 4, row * SQUARE_SIZE + 3 * SQUARE_SIZE // 4), CROSS_WIDTH)
-        pygame.draw.line(screen, color, (col * SQUARE_SIZE + SQUARE_SIZE // 4, row * SQUARE_SIZE + 3 * SQUARE_SIZE // 4), (col * SQUARE_SIZE + 3 * SQUARE_SIZE // 4, row * SQUARE_SIZE + SQUARE_SIZE // 4), CROSS_WIDTH)
-      elif board[row][col] == 2:
-        pygame.draw.circle(screen, color, (int(col * SQUARE_SIZE + SQUARE_SIZE // 2), int(row * SQUARE_SIZE + SQUARE_SIZE // 2)), CIRCLE_RADIUS, CIRCLE_WIDTH)
+  for dim in range(BOARD_DIMENSIONS):
+    for row in range(BOARD_ROWS):
+      for col in range(BOARD_COLS):
+        if board[dim][row][col] == 1:
+          pygame.draw.line(screen, color, (col * SQUARE_SIZE + DIMENSION_SIZE * dim + SQUARE_SIZE // 4, row * SQUARE_SIZE + SQUARE_SIZE // 4), (col * SQUARE_SIZE + DIMENSION_SIZE * dim + 3 * SQUARE_SIZE // 4, row * SQUARE_SIZE + 3 * SQUARE_SIZE // 4), CROSS_WIDTH)
+          pygame.draw.line(screen, color, (col * SQUARE_SIZE + DIMENSION_SIZE * dim + SQUARE_SIZE // 4, row * SQUARE_SIZE + 3 * SQUARE_SIZE // 4), (col * SQUARE_SIZE + DIMENSION_SIZE * dim + 3 * SQUARE_SIZE // 4, row * SQUARE_SIZE + SQUARE_SIZE // 4), CROSS_WIDTH)
+        elif board[dim][row][col] == 2:
+          pygame.draw.circle(screen, color, (int(col * SQUARE_SIZE + DIMENSION_SIZE * dim + SQUARE_SIZE // 2), int(row * SQUARE_SIZE + SQUARE_SIZE // 2)), CIRCLE_RADIUS, CIRCLE_WIDTH)
 
 
-def isSquareAvailable(row, col):
-  return board[row][col] == 0
+#FUNCTIONS
+def isSquareAvailable(dim, row, col):
+  return board[dim][row][col] == 0
 
 
-def markSquare(row, col, player):
-  board[row][col] = player
+def markSquare(dim, row, col, player):
+  board[dim][row][col] = player
 
 
 def isBoardFull(checkBoard = board):
-  for row in range(BOARD_ROWS):
-    for col in range(BOARD_COLS):
-      if checkBoard[row][col] == 0:
-        return False
+  for dim in range(BOARD_DIMENSIONS):
+    for row in range(BOARD_ROWS):
+      for col in range(BOARD_COLS):
+        if checkBoard[dim][row][col] == 0:
+          return False
   return True
 
 
 def checkWin(player, checkBoard = board):
+  for dim in range(BOARD_DIMENSIONS):
+    for row in range(BOARD_ROWS):
+      if np.all(checkBoard[dim][row] == player):
+        return True #HORIZONTAL LINE ON SAME DIMENSION
+
+    for col in range(BOARD_COLS):
+      if np.all(checkBoard[dim][:, col] == player):
+        return True #VERTICAL LINE ON SAME DIMENSION
+      
+    if all(checkBoard[dim][row][row] == player for row in range(BOARD_ROWS)):
+        return True #DIAGONAL LINE ON SAME DIMENSION \
+    
+    if all(checkBoard[dim][row][3 - row] == player for row in range(BOARD_ROWS)):
+        return True #OPPOSITE DIAGONAL LINE ON SAME DIMENSION /
+
   for row in range(BOARD_ROWS):
-    if checkBoard[row][0] == player and checkBoard[row][1] == player and checkBoard[row][2] == player and checkBoard[row][3] == player:
-      return True
+    for col in range(BOARD_COLS):
+      if all(checkBoard[dim][row][col] == player for dim in range(BOARD_DIMENSIONS)):
+        return True #STRAIGHT LINE THROUGH EVERY DIMENSION
+
+  for row in range(BOARD_ROWS):
+    if all(checkBoard[dim][row][dim] == player for dim in range(BOARD_DIMENSIONS)):
+      return True #HORIZONTAL LINE THROUGH DIMENSIONS
+    if all(checkBoard[dim][row][3 - dim] == player for dim in range(BOARD_DIMENSIONS)):
+      return True #OPPOSITE HORIZONTAL LINE THROUGH DIMENSIONS
 
   for col in range(BOARD_COLS):
-    if checkBoard[0][col] == player and checkBoard[1][col] == player and checkBoard[2][col] == player and checkBoard[3][col] == player:
-      return True
+    if all(checkBoard[dim][dim][col] == player for dim in range(BOARD_DIMENSIONS)):
+      return True #VERTICAL LINE THROUGH DIMENSIONS
+    if all(checkBoard[dim][3 - dim][col] == player for dim in range(BOARD_DIMENSIONS)):
+      return True #OPPOSITE VERTICAL LINE THROUGH DIMENSIONS
 
-  if checkBoard[0][0] == player and checkBoard[1][1] == player and checkBoard[2][2] == player and checkBoard[3][3] == player:
-    return True
+  if all(checkBoard[dim][dim][dim] == player for dim in range(BOARD_DIMENSIONS)):
+    return True #DIAGONAL LINE THROUGH DIMENSIONS \
+  if all(checkBoard[dim][dim][3 - dim] == player for dim in range(BOARD_DIMENSIONS)):
+    return True #DIAGONAL LINE THROUGH DIMENSIONS /
 
-  if checkBoard[0][3] == player and checkBoard[1][2] == player and checkBoard[2][1] == player and checkBoard[3][0] == player:
-    return True
+  if all(checkBoard[dim][3 - dim][dim] == player for dim in range(BOARD_DIMENSIONS)):
+    return True #DIAGONAL LINE THROUGH DIMENSIONS /
+  if all(checkBoard[dim][dim][3 - dim] == player for dim in range(BOARD_DIMENSIONS)):
+    return True #DIAGONAL LINE THROUGH DIMENSIONS \
 
   return False
 
 
 def minimax(minimaxBoard, depth, isMaximizing):
-  if depth == 3:
+  if depth == maxDepth:
     return 0
   if checkWin(2, minimaxBoard):
     return float('inf')
@@ -92,41 +132,45 @@ def minimax(minimaxBoard, depth, isMaximizing):
 
   if isMaximizing:
     bestScore = float('-inf')
-    for row in range(BOARD_ROWS):
-      for col in range(BOARD_COLS):
-        if minimaxBoard[row][col] == 0:
-          minimaxBoard[row][col] = 2
-          score = minimax(minimaxBoard, depth + 1, False)
-          minimaxBoard[row][col] = 0
-          bestScore = max(score, bestScore)
+    for dim in range(BOARD_DIMENSIONS):
+      for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLS):
+          if minimaxBoard[dim][row][col] == 0:
+            minimaxBoard[dim][row][col] = 2
+            score = minimax(minimaxBoard, depth + 1, False)
+            minimaxBoard[dim][row][col] = 0
+            bestScore = max(score, bestScore)
     return bestScore
   else:
     bestScore = float('inf')
-    for row in range(BOARD_ROWS):
-      for col in range(BOARD_COLS):
-        if minimaxBoard[row][col] == 0:
-          minimaxBoard[row][col] = 1
-          score = minimax(minimaxBoard, depth + 1, True)
-          minimaxBoard[row][col] = 0
-          bestScore = min(score, bestScore)
+    for dim in range(BOARD_DIMENSIONS):
+      for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLS):
+          if minimaxBoard[dim][row][col] == 0:
+            minimaxBoard[dim][row][col] = 1
+            score = minimax(minimaxBoard, depth + 1, True)
+            minimaxBoard[dim][row][col] = 0
+            bestScore = min(score, bestScore)
     return bestScore
   
 
 def bestMove():
   bestScore = float('-inf')
-  move = (-1, -1)
-  for row in range(BOARD_ROWS):
-    for col in range(BOARD_COLS):
-      if board[row][col] == 0:
-        board[row][col] = 2
-        score = minimax(board, 0, False)
-        board[row][col] = 0
-        if score > bestScore:
-          bestScore = score
-          move = (row, col)
+  move = None
 
-  if move != (-1, -1):
-    markSquare(move[0], move[1], 2)
+  for dim in range(BOARD_DIMENSIONS):
+    for row in range(BOARD_ROWS):
+      for col in range(BOARD_COLS):
+        if board[dim][row][col] == 0:
+          board[dim][row][col] = 2
+          score = minimax(board, 0, False)
+          board[dim][row][col] = 0
+          if score > bestScore:
+            bestScore = score
+            move = (dim, row, col)
+
+  if move:
+    markSquare(move[0], move[1], move[2], 2)
     return True
   return False
 
@@ -134,9 +178,10 @@ def bestMove():
 def restartGame():
   screen.fill(BLACK)
   drawLines(WHITE)
-  for row in range(BOARD_ROWS):
-    for col in range(BOARD_COLS):
-      board[row][col] = 0
+  for dim in range(BOARD_DIMENSIONS):
+    for row in range(BOARD_ROWS):
+      for col in range(BOARD_COLS):
+        board[dim][row][col] = 0
 
 
 drawLines()
@@ -151,11 +196,15 @@ while True:
       sys.exit()
 
     if event.type == pygame.MOUSEBUTTONDOWN and not gameOver:
-      mouseX = event.pos[0] // SQUARE_SIZE
-      mouseY = event.pos[1] // SQUARE_SIZE
+      x, y = event.pos
 
-      if isSquareAvailable(mouseY, mouseX):
-        markSquare(mouseY, mouseX, player)
+      dim = x // DIMENSION_SIZE
+      col = (x % DIMENSION_SIZE) // SQUARE_SIZE
+      row = y // SQUARE_SIZE
+
+
+      if isSquareAvailable(dim, row, col):
+        markSquare(dim, row, col, player)
         if checkWin(player):
           gameOver = True
         player = player % 2 + 1
